521135bf198ed4be212ebbaec8b24978ee5b8840
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-16 20:34:42 +0300

    changes ended time show with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import './App.css';
-
-const App = ()=>{
+import Classwork2 from "./Componentss/classwork2";
+const App = ({name})=>{
 return (
+  <div>
   <h1>Hello world!</h1>
-)
+  <p>Your name {name}</p>
+  <Classwork2 date={ new Date() }/>
+  </div>
+);
 }
-export default App
+export default App;
